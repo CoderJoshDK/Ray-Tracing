0600d465d6721f07bac474081395ece606884f17
CoderJoshDK <>
2021-12-24 18:15:01 -0500

    Start of ray tracing project

---
+++ InOneWeekend/rtweekend.h
@@ -0,0 +1,20 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include<cmath>
+#include<limits>
+#include<memory>
+
+using std::shared_ptr, std::make_shared, std::sqrt;
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+inline double degrees_to_radians(double degrees){
+    return degrees * pi / 180.0;
+}
+
+#include "ray.h"
+#include "vec3.h"
+
+#endif
