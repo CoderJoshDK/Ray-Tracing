cf7ccb231dd3fc5fc102ac74aa3e7d814c999e7e
CoderJoshDK <>
2021-12-24 21:37:46 -0500

    Added antialiasing

--- InOneWeekend/color.h
+++ InOneWeekend/color.h
@@ -5,11 +5,21 @@
 
 #include<iostream>
 
-void write_color(std::ostream &out, color pixel_color){
+void write_color(std::ostream &out, color pixel_color, int samples_per_pixel){
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // Average from the number of smaples;
+    auto scale = 1.0 / samples_per_pixel;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+    
     // Write translated [0,255] value of color compenents.
-    out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-    << static_cast<int>(255.999 * pixel_color.y()) << ' '
-    << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+    out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
 }
 
 #endif
--- InOneWeekend/rtweekend.h
+++ InOneWeekend/rtweekend.h
@@ -4,6 +4,7 @@
 #include<cmath>
 #include<limits>
 #include<memory>
+#include <cstdlib>
 
 using std::shared_ptr, std::make_shared, std::sqrt;
 
@@ -12,6 +13,19 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees){
     return degrees * pi / 180.0;
+}
+
+inline double random_double(){
+    return rand() / (RAND_MAX + 1.0);
+}
+inline double random_double(double min, double max){
+    return min + (max-min)*random_double();
+}
+
+inline double clamp(double x, double min, double max){
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 
 #include "ray.h"
