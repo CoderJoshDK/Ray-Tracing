dde710664c2f0d0f16862c701d969cb7e02a7866
CoderJoshDK <>
2021-12-26 11:42:17 -0500

    Reformatted Files

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# Ray-Tracing Tests
+# Ray Tracing
+
+[![language](https://img.shields.io/badge/-c++17-black?logo=c%2B%2B&17style=social)](https://en.cppreference.com/w/cpp/17)
+![commit](https://img.shields.io/github/last-commit/CoderJoshDK/Ray-Tracing?label=last%20update)
+
+---
 
 ## Impilmented Topics
  - Objects:  
    - Sphere
  - Materials:  
-   - Diffuse
+   - Diffuse (lambertain)
    - Metal
    - Dielectrics
  - Camera
@@ -13,16 +18,19 @@
  - Multithreading
  
 ## Output
-Output is in .ppm format. Right now, to get an output image, run build and run. The exe will output `image.ppm`
+Output is in .ppm format. Right now, to get an output image, run build. The exe will output `image.ppm`.  
+To change the quality of the image, change `samples_per_pixel` or `image_width`.
+![sampleRender1](Images/sampleRender1.png)  
+It is intresting to note that glass spheres do not cast shadows. This is physically correct.
+
 
 ### To-Do
  - BVH
- - Motion Blur 
+ - ***Motion Blur***
  - Textures
  - Rectangles
  - Lights
- - Volumes 
-Eventually, will add things like volumes.
+ - Volumes  
 
 ----------
 ![WIP](https://img.shields.io/static/v1?label=Status&message=WIP&color=red)
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "sphere.h"
 #include "camera.h"
 #include "material.h"
+#include "scenes.h"
 
 
 #include <iostream>
@@ -32,69 +33,6 @@ color ray_color(const ray& r, const hittable& world, int depth){
     return (1.0-t)*color(1.0,1.0,1.0) + t*color(.5,.7,1.0);
 }
 
-hittable_list simple_scene(){
-    hittable_list world;
-
-    auto ground_material = make_shared<lambertain>(color(0.5,0.5,0.5));
-    world.add(make_shared<sphere>(point3(0, -500, 0), 500, ground_material));
-
-    auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
-
-    auto material2 = make_shared<lambertain>(color(0.1, 0.2, 0.5));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
-
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
-
-    return world;
-}
-
-hittable_list random_scene(){
-    hittable_list world;
-
-    auto ground_material = make_shared<lambertain>(color(0.5,0.5,0.5));
-    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
-
-    for (int a = -11; a < 11; a++){
-        for (int b = -11; b < 11; b++){
-            auto choose_mat = random_double();
-            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
-
-            if ((center - point3(4, 0.2, 0)).length() > 0.9){
-                shared_ptr<material> sphere_material;
-
-                if (choose_mat < 0.8){
-                    // Diffuse
-                    auto albedo = color::random() * color::random();
-                    sphere_material = make_shared<lambertain>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                }else if (choose_mat < 0.95){
-                    // Metal
-                    auto albedo = color::random(0.5, 1);
-                    auto fuzz = random_double(0, 0.5);
-                    sphere_material = make_shared<metal>(albedo, fuzz);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                }else{
-                    // Glass
-                    sphere_material = make_shared<dielectric>(1.5);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                }
-            }
-        }
-    }
-
-    auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
-
-    auto material2 = make_shared<lambertain>(color(0.1, 0.2, 0.5));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
-
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
-
-    return world;
-}
 
 struct RayResult{
     unsigned int index;
@@ -108,9 +46,9 @@ int main(int argc, char const *argv[])
 
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 1280;
+    const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 500;
+    const int samples_per_pixel = 100;
     const int max_depth = 50;
 
     color* image = new color[image_width * image_width];
